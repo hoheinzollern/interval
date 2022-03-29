8a813f745cd85a3703cb986868904116414e3469
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2022-03-29 19:13:07 +0200

    Add a helper function for root_intro.

--- src/Tactics/Root_helper.v
+++ src/Tactics/Root_helper.v
@@ -109,6 +109,91 @@ destruct check_goal eqn:Hg.
     easy.
 Qed.
 
+Definition root_contains prec depth hyps px cx pf cf b :=
+  let hyps := R.merge_hyps prec hyps in
+  let xi := nth 0 (A.BndValuator.eval prec px (hyps ++ map (T.eval_bnd prec) cx)) I.nai in
+  refine_root_aux depth prec pf (hyps ++ map (T.eval_bnd prec) cf) xi (fun xi => I.subset xi b).
+
+Theorem root_contains_correct :
+  forall prec depth vars hyps px cx pf cf b,
+  root_contains prec depth hyps px cx pf cf b = true ->
+  eval_hyps hyps vars (eval_real' pf (eval_real' px vars cx :: vars) cf = 0%R ->
+    contains (I.convert b) (Xreal (eval_real' px vars cx ))).
+Proof.
+intros prec depth vars hyps px cx pf cf b H.
+apply (R.eval_hyps_bnd_correct prec).
+set (x := eval_real' px vars cx).
+intros H1 H2.
+unfold root_contains in H.
+revert H.
+set (xi := nth 0 _ _).
+assert (Hx: contains (I.convert xi) (Xreal x)).
+{ apply A.BndValuator.eval_correct'.
+  now apply app_merge_hyps_eval_bnd. }
+clearbody xi.
+revert xi Hx.
+induction depth as [|depth IH].
+easy.
+intros xi Hx.
+simpl.
+fold (compute_inputs prec hyps cf).
+refine (_ (A.DiffValuator.root_correct prec pf (compute_inputs prec hyps cf) _ _ xi x Hx H2)).
+2: now apply app_merge_hyps_eval_bnd.
+set (xi' := A.DiffValuator.root _ _ _ _).
+intros Hx' H.
+destruct I.subset eqn:Hg.
+- clear H IH.
+  now apply I.subset_correct with (2 := Hg).
+- generalize (I.bisect_correct _ _ Hx).
+  destruct (I.bisect xi) as [xi1 xi2].
+  destruct I.wider.
+  + intros _.
+    now apply (IH _ Hx').
+  + destruct refine_root_aux eqn:Hb.
+    now intros [K|K] ; apply (IH _ K).
+    easy.
+Qed.
+
+(* loc = Lt gives the leftmost root, Gt the rightmost one, Eq all of them *)
+Fixpoint root_plain_aux depth prec prog bounds xi loc :=
+  match depth with
+  | S depth =>
+    let xi' := A.DiffValuator.root prec prog bounds xi in
+    if I.is_empty xi' then xi'
+    else
+      let (xi1,xi2) := I.bisect xi in
+      if I.wider prec xi1 xi' then
+        root_plain_aux depth prec prog bounds xi' loc
+      else
+        match loc with
+        | Eq =>
+          let xi1' := root_plain_aux depth prec prog bounds xi1 Lt in
+          if I.is_empty xi1' then
+            root_plain_aux depth prec prog bounds xi2 Eq
+          else
+            let xi2' := root_plain_aux depth prec prog bounds xi2 Gt in
+            if I.is_empty xi2' then
+              I.join xi1' (root_plain_aux depth prec prog bounds xi1 Gt)
+            else I.join xi1' xi2'
+        | Lt =>
+          let xi1' := root_plain_aux depth prec prog bounds xi1 Lt in
+          if I.is_empty xi1' then
+            root_plain_aux depth prec prog bounds xi2 Lt
+          else xi1'
+        | Gt =>
+          let xi2' := root_plain_aux depth prec prog bounds xi2 Gt in
+          if I.is_empty xi2' then
+            root_plain_aux depth prec prog bounds xi1 Gt
+          else xi2'
+        end
+  | O => xi
+  end.
+
+Definition root_plain prec depth hyps px cx pf cf :=
+  let hyps := R.merge_hyps prec hyps in
+  let xi := nth 0 (A.BndValuator.eval prec px (hyps ++ map (T.eval_bnd prec) cx)) I.nai in
+  root_plain_aux depth prec pf (hyps ++ map (T.eval_bnd prec) cf) xi Eq.
+
 Ltac do_root x Zy prec depth native nocheck :=
   massage_goal ;
   let y :=
